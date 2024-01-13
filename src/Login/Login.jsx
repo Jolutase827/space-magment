c50ac4a44cea8f5bb78494ba75e732d8afbcf035
Jose Luis <jolutase104@gmail.com>
2024-01-13 20:27:53 +0100

    Login con error de cors

---
+++ src/Login/Login.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import "./hoja-de-estilos/Login.css";
+import { useForm } from "react-hook-form";
+function Login() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  let errorBD = false;
+  let errorUser = false;
+  const onSubmit = (dataInput, e) => {
+    errorUser = false;
+    errorBD = false;
+    fetch(
+      `http://localhost/Space Managment/servicioUsuarios/service.php?nombre=${dataInput.name}&pwd=${dataInput.pwd}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data != null) {
+          if (data.admin) {
+            console.log('root')
+          }
+        } else {
+          errorUser = true;
+        }
+        errorBD = false;
+      })
+      .catch((error) => {
+        errorBD = true;
+        errorUser = false;
+      });
+  };
+
+  return (
+    <main>
+      <form
+        className="col-12 col-md-8 col-lg-5 rounded pt-3 d-flex flex-column"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="mb-4 ms-4">Log in</h1>
+        <div className="d-flex flex-column align-items-center">
+          <input
+            type="text"
+            placeholder="User name"
+            className="col-8 mb-4 rounded"
+            name="name"
+            {...register("name", {
+              required: "Please, complete all the fieldes",
+            })}
+            aria-invalid={errors.name ? "true" : "false"}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            class="col-8 rounded"
+            name="pwd"
+            {...register("pwd", {
+              required: "Please, complete all the fieldes",
+            })}
+            aria-invalid={errors.pwd ? "true" : "false"}
+          />
+          <button className="button-enter mt-3 rounded" id="button-enter">
+            Enter
+          </button>
+        </div>
+        <div className="col-12 d-flex justify-content-center error">
+          {(errors.name || errors.pwd) && (
+            <div
+              className="alert alert-danger text-center col-4 mt-3 "
+              role="alert"
+            >
+              {" "}
+              {errors.name != undefined
+                ? errors.name.message
+                : errors.pwd.message}
+            </div>
+          )}
+          {!(errors.name || errors.pwd) && errorBD && (
+            <div
+              className="alert alert-danger text-center col-4 mt-3"
+              role="alert"
+            >
+              Sorry, we have probles in the web please try in other time.
+            </div>
+          )}
+          {!(errors.name || errors.pwd) && errorUser && (
+            <div
+              className="alert alert-danger text-center col-4 mt-3"
+              role="alert"
+            >
+              The user name or the password are wrong
+            </div>
+          )}
+        </div>
+        <p className="col-8 ms-4 mt-4 mb-5">
+          If you don't have account or forget your <strong>password</strong>{" "}
+          please contact with the <a href="">adminstrator</a>
+        </p>
+      </form>
+    </main>
+  );
+}
+
+export default Login;
